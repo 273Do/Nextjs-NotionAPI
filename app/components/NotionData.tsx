7dd40820286bac516adb6165be3dc9102c7077e1
273* <keita.y310@icloud.com>
2025-03-25 18:43:03 +0900

    データベースの中身を表示

--- app/components/NotionData.tsx
+++ app/components/NotionData.tsx
@@ -1,3 +1,4 @@
+import { P } from "notion-to-md/build/page-URuPGmoo";
 import React from "react";
 
 const NotionData = async () => {
@@ -5,11 +6,35 @@ const NotionData = async () => {
     const response = await fetch("http://localhost:3000/api");
     const data = await response.json();
     console.log(data);
+
+    return (
+      <>
+        {data.map((post: any) => {
+          return (
+            <div
+              key={post.id}
+              style={{ border: "1px solid white", margin: "10px" }}
+            >
+              <h2 style={{ color: post.color }}>{post.title}</h2>
+              <p>{post.date}</p>
+              <div style={{ display: "flex", gap: "10px" }}>
+                {post.tags.map((tag: string) => {
+                  return <p key={tag}>{tag}</p>;
+                })}
+              </div>
+              {post.files.length ? (
+                <img src={post.files} alt="file" width={100} />
+              ) : (
+                <p>no file</p>
+              )}
+            </div>
+          );
+        })}
+      </>
+    );
   } catch (error) {
     console.error("データの取得に失敗しました:", error);
   }
-
-  return <div>notionData</div>;
 };
 
 export default NotionData;
